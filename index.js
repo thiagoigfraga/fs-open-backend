2433adff2e62c15d4d43febd76b1e7bc9380b1ea
Thiago Garcia <thiagogarciafraga99@gmail.com>
2023-01-20 06:10:15 -0300

    basic express config

---
+++ index.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const app = express();
+
+
+const contacts = [
+  { 
+    "id": 1,
+    "name": "Arto Hellas", 
+    "number": "040-123456"
+  },
+  { 
+    "id": 2,
+    "name": "Ada Lovelace", 
+    "number": "39-44-5323523"
+  },
+  { 
+    "id": 3,
+    "name": "Dan Abramov", 
+    "number": "12-43-234345"
+  },
+  { 
+    "id": 4,
+    "name": "Mary Poppendieck", 
+    "number": "39-23-6423122"
+  }
+]
+
+app.get('/api/contacts', (request, response) => {
+
+  response.json(contacts);
+})
+app.get('/api/info', (request, response) => {
+
+  response.send(`
+  <p>Phonebook has info for ${contacts.length} people</p>
+  <p>${new Date()}</p>
+  `)
+})
+
+
+
+
+
+const PORT = 3001;
+app.listen(PORT, () => {
+  console.log(`App running in port:${PORT}`);
+})
