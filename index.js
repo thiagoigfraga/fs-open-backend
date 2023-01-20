488be19a6dad8da6bf2b95cabaebc2022eee0c19
Thiago Garcia <thiagogarciafraga99@gmail.com>
2023-01-20 06:37:09 -0300

    Update index.js
    3.1 - 3.6 exercises completed

--- index.js
+++ index.js
@@ -1,47 +1,100 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
+app.use(express.json());
 
-const contacts = [
-  { 
-    "id": 1,
-    "name": "Arto Hellas", 
-    "number": "040-123456"
+const generateNewId = () => {
+  return Math.floor(Math.random() * 100000);
+};
+
+let persons = [
+  {
+    id: 1,
+    name: "Arto Hellas",
+    number: "040-123456",
   },
-  { 
-    "id": 2,
-    "name": "Ada Lovelace", 
-    "number": "39-44-5323523"
+  {
+    id: 2,
+    name: "Ada Lovelace",
+    number: "39-44-5323523",
   },
-  { 
-    "id": 3,
-    "name": "Dan Abramov", 
-    "number": "12-43-234345"
+  {
+    id: 3,
+    name: "Dan Abramov",
+    number: "12-43-234345",
   },
-  { 
-    "id": 4,
-    "name": "Mary Poppendieck", 
-    "number": "39-23-6423122"
+  {
+    id: 4,
+    name: "Mary Poppendieck",
+    number: "39-23-6423122",
+  },
+];
+
+app.get("/api/persons", (request, response) => {
+  response.json(persons);
+});
+
+app.get("/api/persons/:id", (request, response) => {
+  const id = Number(request.params.id);
+  const person = persons.find((c) => c.id === id);
+
+  if (!person) {
+    return response.status(400).end();
   }
-]
 
-app.get('/api/contacts', (request, response) => {
+  response.json(person);
+});
 
-  response.json(contacts);
-})
-app.get('/api/info', (request, response) => {
-
+app.get("/api/info", (request, response) => {
   response.send(`
-  <p>Phonebook has info for ${contacts.length} people</p>
+  <p>Phonebook has info for ${persons.length} people</p>
   <p>${new Date()}</p>
-  `)
-})
+  `);
+});
 
+app.delete("/api/persons/:id", (request, response) => {
+  const id = Number(request.params.id);
 
+  persons = persons.filter((c) => c.id !== id);
 
+  response.status(204).end();
+});
 
+app.post("/api/persons", (request, response) => {
+  const body = request.body;
+
+  if (!body.name) {
+    return response.status(404).json({
+      error: "User needs a name",
+    });
+  }
+
+  if (!body.number) {
+    return response.status(404).json({
+      error: "User needs a number",
+    });
+  }
+
+  const alreadyInPhonebook = persons.find((p) => p.name === body.name);
+
+  if (alreadyInPhonebook) {
+    return response.status(404).json({
+      error: "name must be unique",
+    });
+  }
+
+  const person = {
+    id: generateNewId(),
+    name: body.name,
+    number: body.number,
+  };
+
+  persons = persons.concat(person);
+
+  response.json(person);
+});
 
 const PORT = 3001;
 app.listen(PORT, () => {
   console.log(`App running in port:${PORT}`);
-})
+});
